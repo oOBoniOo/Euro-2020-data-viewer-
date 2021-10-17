4084a4bee80e6d6d1d3520db94f0da65eba2b70f
oOBoniOo <boni85@gmail.com>
2021-10-17 21:57:30 +0200

    streamlit

--- dashboard/ini.py
+++ dashboard/ini.py
@@ -2,13 +2,44 @@ import streamlit as st
 from streamlit import caching
 import support.get_data as datos
 import requests
+import pandas as pd
+import matplotlib.pyplot as plt
 
-b1 = st.sidebar.button("Datos Selecciones")
-b2 = st.sidebar.button("G2")
-b3 = st.sidebar.button("G3")
-b4 = st.sidebar.button("G4")
+st.image("img/e20b.png",width=None)
+@st.cache
+def load_matchs():
+    return datos.get_matches()
 
+#partidos = pd.DataFrame(load_matchs())
+#print(partidos)
+#
+goles = datos.get_goals()
+
+col1, col2= st.columns(2)
+
+st.dataframe(goles)
+fig = plt.pie(goles["tot"], labels=goles["team"])
+st.pyplot(fig)
+
+
+#with col2:
+    #fig = goles.plot.pie(y='tot')
+
+    #st.plotly_chart(fig)
+
+
+
+
+b0 = st.sidebar.button("Inicio")
+b1 = st.sidebar.button("Datos Partidos")
 if b1:
     st.title("COSAS CON EQUIPOS")
     print(type(datos.lista_sel))
-    st.multiselect("Elige selecion", datos.lista_sel())      
+    selec = st.multiselect("Elige selecion", datos.lista_sel())  
+
+
+b2 = st.sidebar.button("Datos de Jugadores")
+b3 = st.sidebar.button("Datos de ")
+b4 = st.sidebar.button("G4")
+
+   
--- dashboard/support/get_data.py
+++ dashboard/support/get_data.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pandas as pd
 def get_matches():
     url="http://127.0.0.1:5000/matchs"
     equipos = requests.get(url).json()
@@ -13,3 +14,16 @@ def search_matchs(parametros):
 def lista_sel():
     url = "http://127.0.0.1:5000/matchs/list_sel"
     return requests.get(url).json()
+
+def get_goals():
+    df = pd.DataFrame(get_matches())
+    print(df)
+    g_home = df.groupby(["team_name_home"]).sum().reset_index()
+    g_away = df.groupby(["team_name_away"]).sum().reset_index()
+    new = pd.DataFrame([g_away["team_name_away"],g_away["team_away_score"],g_home["team_home_score"]])
+    new = new.T
+    new["tot"]= new["team_home_score"]+new["team_away_score"]
+    new.rename(columns={'team_home_score': 'home', 
+                           'team_away_score': 'away',
+                           'team_name_away': 'team'}, inplace=True)
+    return new
