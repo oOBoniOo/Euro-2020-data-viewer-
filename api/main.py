648a4aa29dbb258e3d0cc259f5211210205215d8
oOBoniOo <boni85@gmail.com>
2021-10-21 17:47:24 +0200

    docker

--- api/main.py
+++ api/main.py
@@ -4,4 +4,6 @@ from src.controllers.players_ep import app
 from src.controllers.teams_ep import app
 from src.controllers.squads_ep import app
 from src.controllers.player_add_ep import app
-app.run(debug=True)
+import os 
+port = os.getenv("PORT") or 5000
+app.run(debug=True, port=port, host="0.0.0.0")
